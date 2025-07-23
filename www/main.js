5a4bb51929adcf4e041270c6b0bb55c17d05c1e5
Souravlenka <lenkasourav09@gmail.com>
2025-07-23 22:56:42 +0530

    day5

--- www/main.js
+++ www/main.js
@@ -9,4 +9,39 @@ $(document).ready(function (){
             effect: "bounceOut",
         },
     });
+
+    // siri configuration
+
+    var siriWave = new SiriWave({
+    container: document.getElementById("siri-container"),
+    width: 640,
+    height: 200,
+    style:"ios9",
+    amplitude: "1",	
+    speed:"0.30",
+    autostart: true
+    });
+
+    // siri message animation
+    $('.text').textillate({
+        loop: true,
+        sync: true,
+        in:{
+            effect: "fadeInUp",
+            sync: true
+        },
+        out:{
+            effect: "fadeOutUp",
+            sync: true
+
+        },
+    });
+
+
+    // mic button click event
+    $(selector).click(function (e) { 
+        e.preventDefault();
+        
+    });    
+
 });
