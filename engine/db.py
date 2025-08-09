1962fc674b7e68dd58af73050b485e0b861b274d
Souravlenka <lenkasourav09@gmail.com>
2025-08-09 23:01:59 +0530

    commit no 7

---
+++ engine/db.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+conn = sqlite3.connect("jarvis.db")
+cursor = conn.cursor()
+
+query = "CREATE TABLE IF NOT EXISTS sys_command(id integer primary key , name VARCHAR(100), path VARCHAR(1000))"
+cursor.execute(query)
+
+#query = "INSERT INTO sys_command VALUES (null,'Canva','C:\\Users\\HP\\AppData\\Local\\Programs\\Canva\\Canva.exe')"
+#cursor.execute(query)
+#conn.commit()
+
+query = "CREATE TABLE IF NOT EXISTS web_command(id integer primary key , name VARCHAR(100),url VARCHAR(1000))"
+cursor.execute(query)
+
+query = "INSERT INTO web_command VALUES (null,'flipkart','https://www.flipkart.com/')"
+cursor.execute(query)
+conn.commit()
+
